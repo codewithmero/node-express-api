26809b3726b3de891fda602bb3f3b550dbf2e636
Rudra Gaur <irudragaur@gmail.com>
2022-02-04 14:51:50 +0530

    established connection to the database.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+// Establishing database connection
+mongoose.connect('mongodb://localhost:27017/social-network')
+        .then(() => console.log(`Connected to the database successfully!`))
+        .catch(err => console.log(`Couldn't connect to the database, ${err}`));
 
 const app = express();
 
